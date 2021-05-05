50302202cbccf26bba6086d712c6cdc1ecaa8cfd
invincible0 <anurag.srivastava160600@gmail.com>
2021-05-05 19:59:46 +0530

    register route updated

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const secret = process.env.SECRET || 'thisIsDummySecret';
 app.engine('ejs',ejsMate);
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
-
+app.use(express.urlencoded({ extended: true }));
 // app.use(express.static(path.join(__dirname,'public')));
 
 const sessionConfig = {
@@ -32,6 +32,7 @@ const sessionConfig = {
         maxAge: 1000*60*60*24*7
     }
 };
+
 app.use(session(sessionConfig));
 app.use(passport.initialize());
 app.use(passport.session());//session should be used before this
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
 
 router.get('/login',(req,res)=>{
     res.render('users/login');
@@ -8,6 +9,19 @@ router.get('/login',(req,res)=>{
 router.get('/register',(req,res)=>{
     res.render('users/register');
 });
+router.post('/register',async (req,res)=>{
+    try{
+        const {username,email,password} = req.body;
+        const user = new User({email,username});
+        const registeredUser = await User.register(user,password);
+        console.log(registeredUser);
+        res.redirect('/');
+    }catch (e){
+        console.log(e);
+        res.redirect('/register');
+    }
+   
+})
 
 router.get('/logout',(req,res)=>{
     res.redirect('/');
